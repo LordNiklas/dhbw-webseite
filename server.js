41157e0c007a61e40137472cbf8a1e4ee1bd6af3
Niklas <niklaskessler.konto@gmail.com>
2024-09-30 17:31:00 +0200

    Test

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const http = require('http');
- 
+const appContent = require('./client/src/App.js'); // Stelle sicher, dass der Pfad korrekt ist
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Niklas!\n');
+  res.setHeader('Content-Type', 'text/html'); // Ändere den Content-Type zu HTML
+  res.end(appContent); // Sende den Inhalt aus App.js zurück
 });
- 
+
 const port = 3000;
 server.listen(port, () => {
   console.log(`Server läuft auf http://localhost:${port}/`);
